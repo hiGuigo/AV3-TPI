63bb89019fc8d280aac72dfd88d42eb52b0f43d9
guilherme <guilherme.alvarenga021@gmail.com>
2026-05-26 09:31:51 -0300

    feat: + endpoint /testes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,6 +17,7 @@ import { funcionarioRoutes } from "./routes/funcionario.routes";
 import { aeronaveRoutes } from "./routes/aeronave.routes";
 import { etapaRoutes } from "./routes/etapa.routes";
 import { pecaRoutes } from "./routes/peca.routes";
+import { testeRoutes } from "./routes/teste.routes";
 
 // a função Fastify é a responsável por criar o servidor
 // ela é armazenada em um variável para facilidade
@@ -70,6 +71,7 @@ await fastify.register(funcionarioRoutes);
 await fastify.register(aeronaveRoutes);
 await fastify.register(etapaRoutes);
 await fastify.register(pecaRoutes);
+await fastify.register(testeRoutes);
 
 // a inicialização do servidor demorar um pouco, por isso a função é asíncrona
 // definir "host: 0.0.0.0" permite que o servidor aceite conexões externas
